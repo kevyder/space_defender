ed09250ddbfeae56cbd0ac07a9f74f46eca4a37d
Kevin <kyrodriguez97@gmail.com>
2017-01-02 15:01:56 +0000

    Shots

--- game.js
+++ game.js
@@ -1,4 +1,7 @@
 var ship;
+var bullets;
+var timebetweenbullets = 200;
+var time = 0;
 var Game = {
     
     preload: function(){
@@ -14,10 +17,32 @@ var Game = {
         ship = game.add.sprite(game.width/2, 490, 'ship');
         ship.anchor.setTo(0.5);
         game.physics.arcade.enable(ship);
+        bullets = game.add.group();
+        bullets.enableBody = true;
+        bullets.setBodyType = Phaser.Physics.ARCADE;
+        bullets.createMultiple(1500, 'laser');
+        bullets.setAll('anchor.x', 0.5);
+        bullets.setAll('anchor.y', 0.5);
+        bullets.setAll('checkWorldBounds', true);
+        bullets.setAll('outOfBoundsKill', true);
     },
     
     update: function(){
-        
+        ship.rotation = game.physics.arcade.angleToPointer(ship) + Math.PI/2;
+        if (game.input.activePointer.isDown){
+            this.shot();
+        }
+    },
+    
+    shot: function(){
+        if(game.time.now > time && bullets.countDead() > 0){
+            time = game.time.now + timebetweenbullets;
+            var bullet = bullets.getFirstDead();
+            bullet.anchor.setTo(0.5);
+            bullet.reset(ship.x , ship.y);
+            bullet.rotation = game.physics.arcade.angleToPointer(bullet) + Math.PI/2;
+            game.physics.arcade.moveToPointer(bullet, 200);
+        }
     }
     
 };
