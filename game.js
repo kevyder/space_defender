0b037d240d8cbd45d27e415323ee30a2be6fb969
Kevin <kyrodriguez97@gmail.com>
2017-01-02 15:57:58 +0000

    collides & points

--- game.js
+++ game.js
@@ -1,7 +1,11 @@
 var ship;
 var bullets;
-var timebetweenbullets = 200;
+var timebetweenbullets = 300;
 var time = 0;
+var enemies;
+var timer;
+var score;
+var scoretext;
 var Game = {
     
     preload: function(){
@@ -25,13 +29,26 @@ var Game = {
         bullets.setAll('anchor.y', 0.5);
         bullets.setAll('checkWorldBounds', true);
         bullets.setAll('outOfBoundsKill', true);
+        
+        enemies = game.add.group();
+        enemies.enableBody = true;
+        enemies.setBodyType = Phaser.Physics.ARCADE;
+        enemies.createMultiple(200, 'enemy');
+        enemies.setAll('anchor.x', 0.5);
+        enemies.setAll('anchor.y', 0.5);
+        enemies.setAll('checkWorldBounds', true);
+        enemies.setAll('outOfBoundsKill', true);
+        
+        timer = game.time.events.loop(2000, this.createEnemy, this);
+        score = 0;
+        game.add.text(30, 20, "Points:", {font: "14px sans-serif", fill: "#FFF"});
+        scoretext = game.add.text(80, 20, "0", {font: "14px sans-serif", fill: "#FFF"});
     },
     
     update: function(){
         ship.rotation = game.physics.arcade.angleToPointer(ship) + Math.PI/2;
-        if (game.input.activePointer.isDown){
-            this.shot();
-        }
+        if (game.input.activePointer.isDown){ this.shot() }
+        game.physics.arcade.overlap(bullets, enemies, this.collision, null, this);
     },
     
     shot: function(){
@@ -43,6 +60,23 @@ var Game = {
             bullet.rotation = game.physics.arcade.angleToPointer(bullet) + Math.PI/2;
             game.physics.arcade.moveToPointer(bullet, 200);
         }
+    },
+    
+    createEnemy: function(){
+        var enemy = enemies.getFirstDead();
+        var number = Math.floor(Math.random()*10)+1;
+        enemy.reset(number*39, 0);
+        enemy.anchor.setTo(0.5);
+        enemy.body.velocity.y = 100;
+        enemy.checkWorldBounds = true;
+        enemy.outOfBoundsKill = true;
+    },
+    
+    collision: function(b, e){
+        b.kill();
+        e.kill();
+        score++;
+        scoretext.text = score;
     }
     
 };
